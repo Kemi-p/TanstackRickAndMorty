09649b96fcd8274e4cbc9bf2561c03f5f2d5a453
kemi-p <kpole@dvtsoftware.com>
2026-03-13 05:38:50 +0200

    added searchbar functionality

--- src/components/DisplayData.tsx
+++ src/components/DisplayData.tsx
@@ -6,14 +6,20 @@ import { CharacterCard } from "./CharacterCard"
 
  export function DisplayData() {
     const [page, setPage] = useState(1)
+    const [search,setSearch] = useState('')
   const { isPending,error, data } = useQuery({
-    queryKey: ['characterData', page],
+    queryKey: ['characterData', page,search],
     queryFn: () =>
-      fetch(`https://rickandmortyapi.com/api/character?page=${page}`).then((res) =>
+      fetch(`https://rickandmortyapi.com/api/character?page=${page}&name=${search}`).then((res) =>
         res.json()
       ),
       placeholderData: keepPreviousData
   })
+
+  const handleSearch = (value: string) => {
+    setSearch(value)
+    setPage(1) // reset to page 1 on every new search
+  }
 
   if (isPending) return (
     <div className="flex justify-center items-center h-screen">
@@ -25,11 +31,23 @@ import { CharacterCard } from "./CharacterCard"
       <span>Error: {error.message}</span>
     </div>
   )
+  if (data?.error) return (
+    <div role="alert" className="alert alert-warning max-w-sm mx-auto mt-10">
+      <span>No characters found for "{search}"</span>
+    </div>
+  )
   
   const characters: Character[] =data?.results?.slice(0,12) ?? []
 
   return (
     <div>
+        <div>
+            <input type="text"
+            placeholder="search by characters name" 
+            className="input input-bordered w-full max-w-sm"
+            value={search}
+            onChange={e=> handleSearch(e.target.value)}/>
+        </div>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4" >
             {characters.map(character=>(
                 <CharacterCard key={character.id} character={character}/>
@@ -43,7 +61,6 @@ import { CharacterCard } from "./CharacterCard"
 
              <span className="font-semibold">Page {page} of {data?.info?.pages}</span>
 
-           
             <button onClick={() => setPage(page=> page+1)} disabled={page === data?.info?.pages}>
                 Next
             </button>
