bdbe1c07afc06cd76d9cd6904f3b8d4b655d6973
kemi-p <kpole@dvtsoftware.com>
2026-03-14 22:35:03 +0200

    add types for location and episode

--- src/types/type.ts
+++ src/types/type.ts
@@ -8,3 +8,23 @@ export interface Character{
   origin: { name: string }
   location: { name: string }
 }
+
+export interface Location{
+  id: number,
+  name: string,
+  type: string,
+  dimension: string,
+  residents: string[],
+  url: string,
+  created: string
+}
+
+export interface Episode{
+  id: number,
+  name: string,
+  air_date: string,
+  episode: string,
+  characters: string[],
+  url: string,
+  created: string
+}
