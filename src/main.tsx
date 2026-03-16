bdee38db6feda1a669949b51cd6e61f656144cdb
kemi-p <kpole@dvtsoftware.com>
2026-03-16 12:55:09 +0200

    ran prettier, added 2 more earch queries and

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,23 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 1000 * 60,// fresh for 60 sec
+      gcTime:1000 * 60 * 5 //unused cache gonna be cleared after 5 min
+    }
+  }
+});
+
+/**
+ basically you fetch page 1
+  data is FREHS for 60 seconds
+  during 60 sec, navigating away and back = instant, no refetch
+  after 60 sec, data goes STALE
+  next time you vissit page 1, cached data shows immediately + background refetch happens
+ */ 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
